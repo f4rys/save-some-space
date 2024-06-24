aa36146e4fa2ad721c977e1f9d4f3fc4e379ef67
f4rys <looxxon@gmail.com>
2024-06-24 11:49:19 +0200

    Add secret and rate limit

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const session = require('express-session');
 const flash = require('connect-flash');
 const mongoose = require('mongoose');
 const ShortUrl = require('./models/shortUrl');
+const rateLimit = require('express-rate-limit');
 const app = express();
 
 require('dotenv').config();
@@ -18,27 +19,32 @@ mongoose.connect(process.env.MONGODB, {
     console.error('Error connecting to MongoDB Atlas:', err);
 });
 
-app.set('view engine', 'ejs')
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+    standardHeaders: true,
+    legacyHeaders: false,
+});
+app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/static'));
-app.use(express.urlencoded({ extended: false }))
+app.use(express.urlencoded({ extended: false }));
 
 app.use(session({
-    secret: 'your-secret-key',
+    secret: process.env.SERVER_SECRET,
     resave: true,
     saveUninitialized: true
 }));
 
 app.use(flash());
+app.use(limiter);
 
 app.get('/', async (req, res) => {
     const shortUrls = await ShortUrl.find();
     const latestShortUrl = req.flash('latestShortUrl')[0];
-
     res.render('index', { shortUrls: shortUrls, latestShortUrl: latestShortUrl });
 });
 
 app.post('/shortUrls', async (req, res) => {
-
     const existingShortUrl = await ShortUrl.findOne({ short: req.body.shortUrl });
     if (existingShortUrl) {
         return res.sendStatus(409);
@@ -52,7 +58,6 @@ app.post('/shortUrls', async (req, res) => {
     await newShortUrl.save();
 
     req.flash('latestShortUrl', newShortUrl);
-
     res.redirect('/');
 });
 
