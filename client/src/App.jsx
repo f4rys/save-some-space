c24ac4bd470dcfe2b1a64dfac10f92bdfa20a923
f4rys <looxxon@gmail.com>
2024-09-01 16:21:41 +0200

    Delete unnecessary statements

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,5 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import { useState, useEffect } from "react";
 import "./App.css";
-import axios from "axios";
 import Home from "./components/Home.jsx";
 import TermsOfService from "./components/TermsOfService.jsx";
 import PrivacyPolicy from "./components/PrivacyPolicy.jsx";
@@ -9,19 +7,6 @@ import About from "./components/About.jsx";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [array, setArray] = useState([]);
-
-  const fetchAPI = async () => {
-    const response = await axios.get("http://localhost:8080/api");
-    console.log(response.data.shortUrls);
-    setArray(response.data.shortUrls);
-  };
-
-  useEffect(() => {
-    fetchAPI();
-  }, []);
-
   return (
     <>
       <Routes>
