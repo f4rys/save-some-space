8f944bb0e08fdf77277272cd35a27f26c6c70a6f
f4rys <looxxon@gmail.com>
2024-10-05 16:05:04 +0200

    Improve colors

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -4,11 +4,11 @@ import { Link } from "react-router-dom";
 function NavBar() {
   return (
     <>
-      <nav className="navbar bg-body-tertiary assistant-semibold">
+      <nav className="navbar">
         <div className="container-fluid pt-5 px-5">
           <div className="nav-item ps-5">
           <Link to="/">
-            <a className="nav-link">
+            <a className="nav-link assistant-semibold">
               SAVE SOME SPACE.
             </a></Link>
           </div>
@@ -16,21 +16,21 @@ function NavBar() {
           <div className="d-flex pe-5">
             <div className="nav-item mx-5">
             <Link to="/about">
-              <a className="nav-link">
+              <a className="nav-link assistant-semibold">
                 ABOUT
               </a>
               </Link>
             </div>
             <div className="nav-item mx-5">
             <Link to="/terms-of-service">
-              <a className="nav-link" href="/terms-of-service">
+              <a className="nav-link assistant-semibold">
                 TERMS OF SERVICE
               </a>
               </Link>
             </div>
             <div className="nav-item mx-5">
               <Link to="/privacy-policy">
-              <a className="nav-link" href="/privacy-policy">
+              <a className="nav-link assistant-semibold">
                 PRIVACY POLICY
               </a>
               </Link>
