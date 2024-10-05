e55d09eb612c76f3ca03e27681e665a3b57df0ea
f4rys <looxxon@gmail.com>
2024-10-05 15:43:31 +0200

    Add scissors

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,9 +1,20 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import scissors1 from "../assets/scissors1.png";
 
 function Home() {
   return (
     <>
-      <div>Home</div>
+      <div className="row mt-5 mx-5">
+        <div className="col position-relative ms-5">
+          <img className="scissors position-absolute z-3" src={scissors1}></img>
+          <div className="position-absolute">
+            <div className="showdex">SAVE</div>
+            <div className="showdex">SOME</div>
+            <div className="showdex">SPACE.</div>
+          </div>
+        </div>
+        <div className="col"> URL SHORTENING</div>
+      </div>
     </>
   );
 }
