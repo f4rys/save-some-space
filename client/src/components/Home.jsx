9b2bbf010b366b2249b20b053ac6d993a23db766
f4rys <looxxon@gmail.com>
2024-10-06 10:55:38 +0200

    Add settings

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,5 +1,8 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import scissors1 from "../assets/scissors1.png";
+import qr from "../assets/qr.png";
+import clock from "../assets/clock.png";
+import note from "../assets/note.png";
 
 function Home() {
   return (
@@ -15,7 +18,44 @@ function Home() {
         </div>
         <div className="col mt-5">
           <div className="tenor-sans-regular">URL SHORTENER</div>
-          <button className="shorten-button assistant-semibold">SHORTEN</button>
+          <div className="my-4">
+            <input
+              className="url-input px-3 py-2"
+              type="text"
+              placeholder="https://www.example.com/"
+            ></input>
+          </div>
+          <div className="ms-2">
+          <div className="d-flex align-items-center mb-2">
+            <img className="settings-icon me-2" src={qr}></img>
+            <div className="assistant-semibold body-font me-2">QR Code</div>
+              <input
+                type="checkbox"
+                className="checkbox"
+              />
+          </div>
+          <div className="d-flex align-items-center mb-2">
+            <img className="settings-icon me-2" src={clock}></img>
+            <div className="assistant-semibold body-font">
+              Expires in (days):
+            </div>
+            <input
+              className="settings-input expiry-input ms-2 px-3"
+              type="text"
+              placeholder="7"
+            ></input>
+          </div>
+          <div className="d-flex align-items-center mb-2">
+            <img className="settings-icon me-2" src={note}></img>
+            <div className="assistant-semibold body-font">savesome.space/</div>
+            <input
+              className="settings-input custom-url-input ms-2 px-3"
+              type="text"
+              placeholder=""
+            ></input>
+          </div>
+          </div>
+          <button className="shorten-button assistant-semibold mt-3">SHORTEN</button>
         </div>
       </div>
     </>
--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -8,7 +8,7 @@ function NavBar() {
         <div className="container-fluid pt-5 px-5">
           <div className="nav-item ps-5">
           <Link to="/">
-            <a className="nav-link assistant-semibold">
+            <a className="nav-link assistant-semibold header-font">
               SAVE SOME SPACE.
             </a></Link>
           </div>
@@ -16,21 +16,21 @@ function NavBar() {
           <div className="d-flex pe-5">
             <div className="nav-item mx-5">
             <Link to="/about">
-              <a className="nav-link assistant-semibold">
+              <a className="nav-link assistant-semibold header-font">
                 ABOUT
               </a>
               </Link>
             </div>
             <div className="nav-item mx-5">
             <Link to="/terms-of-service">
-              <a className="nav-link assistant-semibold">
+              <a className="nav-link assistant-semibold header-font">
                 TERMS OF SERVICE
               </a>
               </Link>
             </div>
             <div className="nav-item mx-5">
               <Link to="/privacy-policy">
-              <a className="nav-link assistant-semibold">
+              <a className="nav-link assistant-semibold header-font">
                 PRIVACY POLICY
               </a>
               </Link>
